5208e1b8ee8f0eaedbc5e031b9e146a46d93e017
Abitech22 <Abitech22@DESKTOP-C3KU8N9>
2022-06-22 17:32:54 +0600

    Добавьте файлы проекта.

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Data;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //[HttpPost]
+        //public string GetList()
+        //{
+        //    return "asd";
+        //}
+
+        [HttpPost]
+        public JsonResult GetList(List<string> list)
+        {
+            var res = _context.Add(list);
+            return Json(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Test/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Test.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
